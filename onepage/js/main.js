db98930ae50a3ad15f375673c19709b98ed01b97
Alan Trope <atrope@gmail.com>
2019-02-27 01:35:38 +0200

    adding new view

--- onepage/js/main.js
+++ onepage/js/main.js
@@ -69,13 +69,70 @@ const doGraphRecipes = () => {
 }
 const doGraphDiseases = () => {
 
-  // svg = d3.select("#d3-diseases").append("div").attr("class", "chart")
-      // .style("width", w + "px").style("height", h + "px")
-      // .append("svg:svg").attr("width", w)
-      // .attr("height", h).append("svg:g")
-      // .attr("transform", "translate(.5,.5)");
+  var width = screen.availWidth / 3 * 2;
+      height = 480;
+
+  var cluster = d3.layout.cluster()
+      .size([height, width - 160]);
+
+  var diagonal = d3.svg.diagonal()
+      .projection(function(d) { return [d.y, d.x]; });
+
+  var svg = d3.select("#d3-diseases").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+    .append("g")
+      .attr("transform", "translate(40,0)");
+      d3.json("js/test.json", function(data) {
+
+        console.lg
+        var nodes = cluster.nodes(data),
+            links = cluster.links(nodes);
+
+        nodes.forEach(function(d) {
+          d.y = d.depth * 200;
+          d.x*=2;
+        });
+
+        var link = svg.selectAll(".link")
+            .data(links)
+          .enter().append("path")
+            .attr("class", "link")
+            .attr("d", diagonal);
+
+        var node = svg.selectAll(".node")
+            .data(nodes)
+          .enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+
+        node.append("circle")
+            .attr("r", 4.5);
+
+        node.append("text")
+            .attr("dx", function(d) { return d.children ? -8 : 8; })
+            .attr("dy", 3)
+            .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
+            .text(function(d) { return d.name; });
+
+});
 
 }
+
+function changed() {
+  timeout = clearTimeout(timeout);
+  (this.value === "tree" ? tree : cluster)(root);
+  var t = d3.transition().duration(750);
+  node.transition(t).attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+  link.transition(t).attr("d", diagonal);
+}
+function diagonal(d) {
+return "M" + d.y + "," + d.x
+    + "C" + (d.parent.y + 100) + "," + d.x
+    + " " + (d.parent.y + 100) + "," + d.parent.x
+    + " " + d.parent.y + "," + d.parent.x;
+}
+
 const doGraphIngredients = () => {
     w = screen.availWidth / 3 * 2,
         h = 480,
