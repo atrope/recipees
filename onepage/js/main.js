64149db7ab0d94c3396624efa11e8532bddb8421
fabianrk52 <fabianrk@gmail.com>
2019-02-26 02:10:42 +0200

    doGraph_diseases try

--- onepage/js/main.js
+++ onepage/js/main.js
@@ -182,6 +182,147 @@ const doGraph_ingredients = () => {
 }
 
 const doGraph_diseases = () => {
+    var margin = {top: 20, right: 50, bottom: 30, left: 20},
+        width = 1200 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+
+    d3.json("/recipees/onepage/js/diseases.json", function(data) {
+        children = data.children.map(function(a){ 
+            return a;
+        });
+
+    var dataset = children.map(function(c) {
+        return {x:c.name,
+                y:c.ingredients.map(function(d){
+                return d;
+                })}
+        })
+        
+    var ingredients = dataset[0].y.map(function(a){
+        return a.name;
+    })
+
+    console.log(ingredients);
+
+    var colors = ["b33040", "#d25c4d", "#f2b447", "#d9d574", "blue", "grey"];
+    
+    var x = d3.scale.ordinal()
+        .domain(dataset.map(function(d) { return d.x; }))
+        .rangeRoundBands([10, width-10], 0.02);
+      
+    var y = d3.scale.linear()
+        .domain([0, d3.max(dataset, function(d) {  return d3.max(d, function(d) { return d.y0 + d.y; });  })])
+        .range([height, 0]);
+
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+    
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("right");
+
+    var svg = d3.select("#d3-uk-diseases")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    
+   
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(5)
+            .tickSize(-width, 0, 0)
+            .tickFormat( function(d) { return d } );
+
+            var xAxis = d3.svg.axis()
+                .scale(x)
+                .orient("bottom")
+
+                svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis);
+
+                svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
+
+        var groups = svg.selectAll("g.value")
+            .data(dataset)
+            .enter().append("g")
+            .attr("class", "value")
+            .style("fill", function(d, i) { return colors[i]; });
+
+            var rect = groups.selectAll("rect")
+            .data(function(d) { return d; })
+            .enter()
+            .append("rect")
+            .attr("x", function(d) { return x(d.x); })
+            .attr("y", function(d) { return y(d.y0 + d.y); })
+            .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); })
+            .attr("width", x.rangeBand())
+            .on("mouseover", function() { tooltip.style("display", null); })
+            .on("mouseout", function() { tooltip.style("display", "none"); })
+            .on("mousemove", function(d) {
+                var xPosition = d3.mouse(this)[0] - 15;
+                var yPosition = d3.mouse(this)[1] - 25;
+                tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+                tooltip.select("text").text(d.y);
+            });
+
+        var legend = svg.selectAll(".legend")
+            .data(colors)
+            .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function(d, i) { return "translate(30," + i * 20 + ")"; });
+            
+            legend.append("rect")
+            .attr("x", width - 18)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", function(d, i) {return colors.slice().reverse()[i];});
+            
+            legend.append("text")
+            .attr("x", width + 5)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .style("text-anchor", "start")
+            .text(function(d, i) { 
+                switch(i){
+                    case 0: return ingredients[0];
+                    case 1: return ingredients[1];
+                    case 2: return ingredients[2];
+                    case 3: return ingredients[3];
+                    case 4: return ingredients[4];
+                    case 5: return ingredients[5];
+                    case 6: return ingredients[6];
+                    case 7: return ingredients[7];
+                    }
+                });
+
+        var tooltip = svg.append("g")
+            .attr("class", "tooltip")
+            .style("display", "none");
+                
+            tooltip.append("rect")
+            .attr("width", 30)
+            .attr("height", 20)
+            .attr("fill", "white")
+            .style("opacity", 0.5);
+
+            tooltip.append("text")
+            .attr("x", 15)
+            .attr("dy", "1.2em")
+            .style("text-anchor", "middle")
+            .attr("font-size", "12px")
+            .attr("font-weight", "bold");
+        });
 }
 
 const doGraph_recipes = () => {
